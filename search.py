15efd69752599da41abcdd077fa6f63d9721c57a
gabe <ggetzie@gmail.com>
2020-11-20 17:19:37 +0800

    search lambda

--- search.py
+++ search.py
@@ -1,5 +1,6 @@
 import boto3
 import json
+import os
 import requests
 from requests_aws4auth import AWS4Auth
 
@@ -8,9 +9,9 @@ service = 'es'
 credentials = boto3.Session().get_credentials()
 awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
 
-host = 'https://search-geography-innovation-hg66af3hqcqsvj4pdsll72p4em.us-east-1.es.amazonaws.com/' 
-index = 'papers'
-url = 'https://' + host + '/' + index + '/_search'
+host = 'search-geography-innovation-hg66af3hqcqsvj4pdsll72p4em.us-east-1.es.amazonaws.com' 
+# index = 'papers'
+url = 'https://' + host + '/_search'
 
 # Lambda execution starts here
 def handler(event, context):
@@ -22,7 +23,7 @@ def handler(event, context):
         "query": {
             "multi_match": {
                 "query": event['queryStringParameters']['q'],
-                "fields": ["fields.paper_title^4", "fields.fos_name^2", "fields.affiliation_name", "fields.author_name"]
+                "fields": ["paper_title^4", "fos_name^2", "affiliation_name", "author_name"]
             }
         }
     }
@@ -31,7 +32,8 @@ def handler(event, context):
     headers = { "Content-Type": "application/json" }
 
     # Make the signed HTTP request
-    r = requests.get(url, auth=awsauth, headers=headers, data=json.dumps(query))
+    auth = (os.environ["ES_USER"], os.environ["ES_PW"])
+    r = requests.get(url, auth=auth, headers=headers, data=json.dumps(query))
 
     # Create the response and add some extra content to support CORS
     response = {
